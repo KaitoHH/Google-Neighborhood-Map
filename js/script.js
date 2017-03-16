6d3a0cfc85dc5de703cd7f4da572c6c1768a417b
KaitoHH <hh.kaito@gmail.com>
2017-03-16 16:08:04 +0800

    complete structure

---
+++ js/script.js
@@ -0,0 +1,22 @@
+(function() {
+    var $expend = $('#expend');
+    var $menu = $('#menu');
+    var $board = $('#board');
+    var isExpend = true;
+
+    // toogle menu
+    $expend.click(function() {
+        isExpend = !isExpend;
+        if (isExpend) {
+            $menu.removeClass('unexpend')
+            $menu.addClass('expend');
+            $board.removeClass('expend');
+            $board.addClass('unexpend');
+        } else {
+            $board.removeClass('unexpend');
+            $board.addClass('expend');
+            $menu.removeClass('expend');
+            $menu.addClass('unexpend');
+        }
+    })
+})();
